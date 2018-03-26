efddfa5918c64c47063308b1645bf94156831271
Sebastian Norlin <wazazew@gmail.com>
2018-03-26 15:32:08 +0200

    person now stores number of tests and added toString

--- python/person.py
+++ python/person.py
@@ -1,6 +1,11 @@
 class Person:
-    testTime = []
-    def __init__(self, name, testTime, hasComputer):
+    
+    def __init__(self, name, testTimes, hasComputer):
         self.name = name
-        self.testTime = self.testTime.append(testTime)
+        self.numberOfTests = 1
+        self.testTime = []
+        self.testTime.append(testTimes)
         self.hasComputer = hasComputer
+
+    def __str__(self):
+        return "Name = %s, number of tests = %s, has computer = %s, TestTimes = %s\n" % (self.name, self.numberOfTests, self.hasComputer, self.testTime)
