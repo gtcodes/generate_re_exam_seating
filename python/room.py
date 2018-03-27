56a1a0100464466ec718eaa156621c181332c22b
Sebastian Norlin <wazazew@gmail.com>
2018-03-27 17:07:00 +0200

    Added functionality for seating plans
    The rooms now have functions to create tables in the seating plan. These
    are tikz nodes in a latex tikz graph. They can either be split tables
    seating two students using the splitTable function or a whole table
    seating only one student using the wholeTable function.
    The create table function is a helper function for both of the above
    functions.
    createSeating should be an abstract method implemented by each room
    speciifying how to divide some set of students.

--- python/room.py
+++ python/room.py
@@ -1,12 +1,45 @@
-class Room:
+#from abc import ABC, abstractmethod
+
+class Room():
         
+    tableWidth = 3
+    tableHeight = 1
+
     def __init__(self, name, col, row):
         self.name=name
-        self.col=col
-        self.row=row
+        self.cols=col
+        self.rows=row
+        self.students=[]
     
     def optNumberOfPeople(self):
-        return self.row*self.col
+        return self.rows*self.cols
 
     def maxNumberOfPeople(self):
         return (2*self.optNumberOfPeople())
+
+    #@abstractmethod
+    def createSeating(self):
+        currentStudent = 0
+        tableCode = ''
+        for row in range(0,self.rows):
+            for table in range(0,self.cols):
+                if(row%2 == 0):
+                    tableCode += self.splitTable(row, table)
+                else:
+                    tableCode += self.wholeTable(row, table)
+        return tableCode
+
+    def wholeTable(self, row, col):
+        widthOffs = col * (self.tableWidth + 1) + (self.tableWidth/4) #divide by 4, half of a half table
+        heightOffs = row * (- self.tableHeight - 1)
+        return self.createTable(self.tableWidth, self.tableHeight, widthOffs, heightOffs, 'bertil')
+
+    def splitTable(self, row, col):
+        widthOffs = col * (self.tableWidth + 1) #dup
+        heightOffs = row * (- self.tableHeight - 1) #dup
+        returnString = self.createTable(self.tableWidth/2, self.tableHeight, widthOffs, heightOffs, 'adam')
+        returnString += self.createTable(self.tableWidth/2, self.tableHeight, widthOffs + self.tableWidth/2, heightOffs, 'adam')
+        return returnString
+
+    def createTable(self, width, height, widthOffs, heightOffs, name):
+         return '\n\\node [draw, minimum width=' + str(width) + 'cm, minimum height='+ str(height) + 'cm, anchor=base] at(' + str(widthOffs) + ',' + str(heightOffs) + ') {\\begin{varwidth}{1.5cm}' + name + '\\end{varwidth}};'
---
+++ python/test.py
@@ -0,0 +1,5 @@
+from room import Room
+
+a = Room("testRoom",6,6)
+
+print(a.createSeating())
